a5036af65247c7c2b64c4549cffce3422e70cf92
petertechy <petertechy01@gmail.com>
2025-03-18 12:36:12 +0100

    sign in updatd

--- src/pages/SignIn.jsx
+++ src/pages/SignIn.jsx
@@ -1,11 +1,48 @@
-import React from 'react'
-
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
 const SignIn = () => {
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  
+  const navigate = useNavigate()
+
+  const handleLogin = () =>{
+    let url = "http://localhost:5700/signin"
+    let userLogin = {email, password}
+    axios.post(url, userLogin)
+    .then((response)=>{
+      console.log(response)
+      if(response.data.status){
+      navigate("/dashboard")
+      }
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+    // console.log({email, password})
+  }
   return (
     <>
-        <h1>Sign In Page</h1>
-    </>
-  )
-}
 
-export default SignIn
+      <div className='mx-auto col-7 rounded-3 shadow p-3 my-4'>
+      <h1 className="text-success text-center">Sign In Page</h1>
+        <input
+          type="text"
+          placeholder="email"
+          onChange={(e) => setemail(e.target.value)}
+          className="form-control mb-3"
+          />
+        <input
+          type="text"
+          placeholder="password"
+          onChange={(e) => setpassword(e.target.value)}
+          className="form-control mb-3"
+        />
+        <button className="btn btn-success w-100" onClick={handleLogin}>Login</button>
+      </div>
+    </>
+  );
+};
+
+export default SignIn;
