8c568af3267502d1609191f2334cd3e552b772fd
petertechy <petertechy01@gmail.com>
2025-03-20 15:10:59 +0100

    src/App.jsx updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
 import Dashboard from "./pages/Dashboard";
+
+let token = localStorage.token
 
 
 const App = () =>{
@@ -12,7 +14,7 @@ const App = () =>{
         <Route path="/" element={<Home/>}/>
         <Route path="sign-up" element={<SignUp/>}/>
         <Route path="sign-in" element={<SignIn/>}/>
-        <Route path="dashboard" element={<Dashboard/>}/>
+        <Route path="dashboard" element={token ? <Dashboard/> : <Navigate to="/sign-in"/>}/>
       </Routes>
     </Router>
   )
