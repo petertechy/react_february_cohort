6e7c92f5836b5dafec03b0fd3db93de9b60ae4d1
petertechy <petertechy01@gmail.com>
2025-03-20 15:11:23 +0100

    src/pages/Dashboard.jsx updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,6 +1,38 @@
-import React from 'react'
-
+import axios from 'axios'
+import React, { useEffect } from 'react'
+import { useNavigate } from 'react-router-dom'
+useNavigate
 const Dashboard = () => {
+  useEffect(() => {
+    getDashboard()
+  }, [])
+
+  let navigate = useNavigate()
+
+  let token = localStorage.token
+  let url = "http://localhost:5700/dashboard"
+
+const getDashboard = () =>{
+  axios.get(url,{
+      headers: {
+       " Authorization": `Bearer ${token}`,
+        "Content-Type": "application/json",
+        "Accept" : "application/json"
+      }
+  })
+  .then((response)=>{
+    if(!response.data.status){
+      console.log(response.data.message)
+      localStorage.removeItem(token)
+      navigate('/sign-in')
+    }
+  })
+  .catch(()=>{
+
+  })
+}
+
+
   return (
     <>
         <h1>Welcome to the Dashboard</h1>
