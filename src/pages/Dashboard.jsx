a87fc353dcbb58a5f009e5e4e6d4f9cb696d1621
petertechy <petertechy01@gmail.com>
2025-03-24 12:11:23 +0100

    dashboard updated

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,43 +1,72 @@
-import axios from 'axios'
-import React, { useEffect } from 'react'
-import { useNavigate } from 'react-router-dom'
-useNavigate
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
+useNavigate;
 const Dashboard = () => {
+  const [dashboardinfo, setdashboardinfo] = useState([])
   useEffect(() => {
-    getDashboard()
-  }, [])
+    getDashboard();
+  }, []);
 
-  let navigate = useNavigate()
+  let navigate = useNavigate();
 
-  let token = localStorage.token
-  let url = "http://localhost:5700/dashboard"
+  let token = localStorage.token;
+  let url = "http://localhost:5700/dashboard";
 
-const getDashboard = () =>{
-  axios.get(url,{
-      headers: {
-       " Authorization": `Bearer ${token}`,
-        "Content-Type": "application/json",
-        "Accept" : "application/json"
-      }
-  })
-  .then((response)=>{
-    if(!response.data.status){
-      console.log(response.data.message)
-      localStorage.removeItem(token)
-      navigate('/sign-in')
-    }
-  })
-  .catch(()=>{
+  const getDashboard = () => {
+    axios
+      .get(url, {
+        headers: {
+          " Authorization": `Bearer ${token}`,
+          "Content-Type": "application/json",
+          Accept: "application/json",
+        },
+      })
+      .then((response) => {
+        if (!response.data.status) {
+          // console.log(response);
+          localStorage.removeItem("token");
+          navigate("/sign-in");
+        }else{
+          console.log(response)
+          setdashboardinfo([response.data.user])
+        }
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
 
-  })
-}
-
+  const handleLogout = () =>{
+    localStorage.removeItem("token")
+    navigate("/sign-in")
+  }
 
   return (
     <>
-        <h1>Welcome to the Dashboard</h1>
-    </>
-  )
-}
+      <div className="border shadow p-3 ">
+        <div className="row justify-center align-center">
+        <div className="col">
+          <h1 className="text-success fs-5">Welcome to the Dashboard</h1>
+        </div>
+        <div className="col text-end">
+          <button onClick={handleLogout} className="btn btn-warning">Logout</button>
+        </div>
+        </div>
 
-export default Dashboard
+        <div>
+          {dashboardinfo.map((user, index)=>(
+            <div key={index}>
+              <h1>{`${user.firstname} ${user.lastname}`}</h1>
+              <p>{user.age}</p>
+              <p>Date Registered: {user.date}</p>
+            </div>
+          ))}
+        </div>
+        
+      </div>
+    </>
+  );
+};
+
+export default Dashboard;
