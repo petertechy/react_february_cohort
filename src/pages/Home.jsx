94e3bf249f9dde786dd09570676b4f248a1248fd
petertechy <petertechy01@gmail.com>
2025-04-17 12:51:29 +0100

    updated link

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
     <>
+    <div>
+      <Link to="/">Home</Link>|
+      <Link to="/sign-up">Register</Link>|
+      <Link to="/sign-in">Sign In</Link>|
+      {/* <Link to="/effect">Use Effect</Link> */}
+
+    </div>
        <div className='p-3'>
        <h1 className='text-danger'>Homepage</h1>
        <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aperiam maxime harum odit iure, sit minus accusantium sapiente maiores beatae dolore et? Iure quibusdam tempore at facere ullam excepturi, autem delectus.</p>
